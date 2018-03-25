43c0c0a1b35ef7d187ff58a1ede470d16e05b8f3
Ashiq <ashiq.r31@gmail.com>
2018-03-25 18:29:14 -0500

    added index.html and koa-router

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 require('babel-register')
 
 const Koa = require('koa')
-const app = new Koa()
+const Router = require('koa-router')
+const serve = require('koa-static')
 
-app.use(async ctx => {
-  ctx.body = 'Hello Hacker'
-})
+const app = new Koa()
+const router = new Router()
+
+// example of route 
+router.get('/example/:hacker', (ctx, next) => ctx.body = `Hello ${ctx.params.hacker}`)
+
+// serves default html
+app
+  .use(serve('./public'))
+  .use(router.routes())
+
 
 app.listen(3000)
 console.log('Running server at port 3000')
