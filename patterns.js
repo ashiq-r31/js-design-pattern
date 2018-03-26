251248b3e34ead81f51f1f6c7718f81070703465
Ashiq <ashiq.r31@gmail.com>
2018-03-25 20:16:21 -0500

    changes

--- patterns.js
+++ patterns.js
@@ -1,9 +1,9 @@
-// factory functions 
+// Factory function 
 
 const factory = ({ arg1 = 1, arg2 = 2 } = {}) => ({
   arg1,
   arg2,
-  method: (arg1, arg2) => {
+  changeParams: (arg1, arg2) => {
     this.ar1 = arg1
     this.arg2 = arg2
     return this
