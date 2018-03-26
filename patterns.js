ace0452a968a8ef76ef74d17b3bc1480868c21a1
Ashiq <ashiq.r31@gmail.com>
2018-03-25 20:23:33 -0500

    change binding

--- patterns.js
+++ patterns.js
@@ -3,7 +3,7 @@
 const factory = ({ arg1 = 1, arg2 = 2 } = {}) => ({
   arg1,
   arg2,
-  changeParams: (arg1, arg2) => {
+  changeParams(arg1, arg2) {
     this.ar1 = arg1
     this.arg2 = arg2
     return this
