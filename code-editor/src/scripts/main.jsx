76fba9e60967034e5ad6fe79b816e5ee4f2cc118
rahmaas <ash.rahman@verizonwireless.com>
2018-04-02 21:15:02 -0500

    added basic components to test

--- code-editor/src/scripts/main.jsx
+++ code-editor/src/scripts/main.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import { render } from 'react-dom'
 import 'grommet/scss/vanilla/index.scss'
-import Editor from './components/Editor'
-import Section from 'grommet/components/Section'
+import App from 'grommet/components/App'
 import Button from 'grommet/components/Button'
-
+import Headline from 'grommet/components/Headline'
+import Heading from 'grommet/components/Heading'
+import Hero from 'grommet/components/Hero'
+import Box from 'grommet/components/Box'
+import Tabs from 'grommet/components/Tabs'
+import Tab from 'grommet/components/Tab'
 // Render editor
 render(
   <div>
-    <Button primary label='Start' />
-    <Section>
-      What's up?
-    </Section>
+    <Tabs> 
+      <Tab title='First'></Tab>
+      <Tab title='Second'></Tab>
+    </Tabs>
+    <App>
+      <Hero>
+      <Heading>Turn your code into poetry</Heading>
+      <Heading margin='medium' tag='h3'>Learn popular Javascript design patterns used in popular libraries and frameworks</Heading>
+      <Box direction='row' wrap align='start' pad={{ between: 'medium' }}>
+        <Button primary label='Start lesson' type='submit' />
+        <Button secondary label='About us' type='submit' />
+      </Box>
+      </Hero>
+    </App>
     {/* <Editor /> */}
   </div>,
   document.getElementById('example')
